c0059a9cda3d8739eeb978476bf1c0ac56b95d7f
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-01-20 13:54:59 +0530

    2

---
+++ p2.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main(){
+  // print("welcome to Dart!");
+  // var name = stdin.readLineSync();
+  // print(name);
+  int a;
+  a = 5;
+  print (a);
+
+  String x = "shaurya";
+  print(x);
+
+  BigInt longValue;
+  longValue = BigInt.parse('9457130429492345083045724370397320');
+  print(longValue);
+
+  Human();
+}
+
+class Human{
+
+}
